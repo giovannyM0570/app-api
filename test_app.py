4eb09eede0b82221d19b8b26168f1b0eff83baad
giovannym0570 <giovannymartha22@hotmail.com>
2025-05-22 12:07:22 +0200

    'test8'

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from main import app
 def test_home():
     client = app.test_client()
     response = client.get('/')
-    assert response.data == b"hallois mijn eerste project"
+    assert response.data == b"hallo dit is mijn eerste project"
