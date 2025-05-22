cb3ddb228d4b759b72d3ad9e8cef365612d3972d
giovannym0570 <giovannymartha22@hotmail.com>
2025-05-22 13:53:11 +0200

    'frontEnd'

--- test_app.py
+++ test_app.py
@@ -2,7 +2,5 @@ from main import app
 
 def test_home():
     client = app.test_client()
-    response = client.get('/')
-    assert response.data == b"hallo dit is mijn eerste project"
     response2 = client.get('/test')
     assert response2.data == b"hallo"
