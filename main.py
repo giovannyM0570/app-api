2910b25dfed7f5e948c7205ac9d1bd111f871b6d
giovannym0570 <giovannymartha22@hotmail.com>
2025-05-22 15:17:22 +0200

    test

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    url = "https://giovannymartha.nl"
-    return url
+    return render_template("index.html")
 
 @app.route("/test")
 def test():
-    return "halasaslo"
+    return redirect("https://giovannymartha.nl")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render geeft poort door via environment variable
