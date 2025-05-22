74548fb41012ee6e78d140c0a4fd360b75466290
giovannym0570 <giovannymartha22@hotmail.com>
2025-05-22 11:51:16 +0200

    'test'

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "hallo dit is mijn eerste projectt"
+def home()
+    return "hallo dit is mijn eerste project"
 
 if __name__ == "__main__":
     app.run()
